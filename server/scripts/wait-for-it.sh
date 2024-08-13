dbe8a06d82e600c3a6a39ead673f2e0f798b6b10
EtherDaler <daler.hojimatov@yandex.ru>
2024-08-13 23:05:19 +0500

    del serv path in script

--- server/scripts/wait-for-it.sh
+++ server/scripts/wait-for-it.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -o server/wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-chmod +x server/wait-for-it.sh
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
