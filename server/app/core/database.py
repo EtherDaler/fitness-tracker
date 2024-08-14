2179e0ee93fcc271eafff6e48bf1a3a849536b1c
EtherDaler <daler.hojimatov@yandex.ru>
2024-08-15 03:52:24 +0500

    add asyncpg conn

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 
 from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 print(DATABASE_URL)
 
 engine = create_async_engine(
