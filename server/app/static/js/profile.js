ff62489b05a29229e7730c659373f835a5c1c039
Dalik <daler.safarovv@yandex.ru>
2024-09-15 22:41:20 +0300

    fix

--- server/app/static/js/profile.js
+++ server/app/static/js/profile.js
@@ -69,7 +69,7 @@ fileInput.addEventListener("change", async (event) => {
   const method = hasAvatar ? "PUT" : "POST";
 
   const res = await fetch("/api/v1/users/photo", {
-    method: PUT
+    method: method,
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
